c6afb63bacec61568204626d25003bcaab202256
Kevin Caballas <kevincaballas@gmail.com>
2018-03-25 22:06:33 +0800

    Add Routertest page and its corresponding entry in the router

---
+++ src/Routertest.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="test-router">
+		
+	<b-container>
+		<b-row>
+			<b-col>
+				<h3 class="text-center">This is a new page tbh</h3>
+			</b-col>
+		</b-row>
+	</b-container></div>
+</template>
+
+<script>
+	export default {
+		name: 'Routertest'
+	}
+</script>
+
+<style>
+	
+</style>
